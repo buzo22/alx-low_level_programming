2a46959e19c5ff268c4f073d1ba04d5add826c1f
buzo22 <chibuzoakana@yahoo.com>
2022-09-08 08:40:17 -0700

    C program that prints exactly the phrase

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-
 /**
- * main - prints the phrase with proper grammar,
- *
- * but the outcome is a piece of art,
- *
+ * main - main block
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
