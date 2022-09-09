aec171a6a3f0d97f31fc60672675dd98c432cda3
buzo22 <chibuzoakana@yahoo.com>
2022-09-09 11:01:57 -0700

    Patience, persistence and perspiration make an unbeatable connection for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,9 +16,8 @@ int main(void)
 	{
 		putchar(i + '0');
 		if (i < 9)
-		{
-																			putchar(44);
-																			putchar(32);
+		{																	putchar(44);
+			putchar(32);
 																		}
 		i++;
 	}
